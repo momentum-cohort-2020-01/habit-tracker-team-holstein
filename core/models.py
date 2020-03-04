302cfd091a0e7393121cfe36e8a47774b185ac4a
Ryan Graham <ryangrahamwpb@gmail.com>
2020-03-04 17:02:25 -0500

    Add field to model.

--- core/models.py
+++ core/models.py
@@ -6,10 +6,11 @@ from django.utils.text import slugify
 class Habit(models.Model):
     name = models.CharField(max_length=100)
     goal = models.IntegerField()
+    unit = models.CharField(max_length=50, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
 
-        return f"Activity {self.name} Target-Goal {self.goal}"
+        return f"Activity {self.name} Target-Goal {self.goal} Unit {self.unit}"
    
