4cf47803c19b9ef3e751423456d194bc1f0870b0
Ryan Graham <ryangrahamwpb@gmail.com>
2020-03-04 15:01:37 -0500

    Add boiler-plate files.

--- core/models.py
+++ core/models.py
@@ -3,3 +3,13 @@ from django.db import models
 from django.utils.text import slugify
 
 # Create your models here.
+class Habit(models.Model):
+    name = models.CharField(max_length=100)
+    goal = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+
+        return f"Activity {self.name} Target-Goal {self.goal}"
+   
--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpRequest
+from .models import Habit
 # Create your views here.
+
+
+def habit_list(request):
+    habit = Habit.objects.all()
+    return render(request, 'core/habit_list.html', {})
